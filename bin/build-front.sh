d05067fbc1e2c844413a04688dff7896c243c690
Kwangsun Noh <nohkwangsun@gmail.com>
2023-04-06 03:19:16 +0800

    add npm install script to build-front.sh file

--- bin/build-front.sh
+++ bin/build-front.sh
@@ -3,6 +3,11 @@
 FRONT_HOME=${FRONT_HOME:-"$(cd "$(dirname "$0")"/../front || exit; pwd)"}
 cd "${FRONT_HOME}" || exit
 
+if ! npm install; then
+  echo "Build failed. : while npm install";
+  exit 1;
+fi
+
 if ! npm run build; then
   echo "Build failed.";
   exit 1;
